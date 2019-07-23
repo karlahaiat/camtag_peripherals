04d1f556129ebaa2709f98462ce67c617068c3d3
karlahaiat <karlahaiat@gmail.com>
2019-07-23 19:16:17 -0400

    Changed USBON.PY

--- power/usbon.py
+++ power/usbon.py
@@ -3,12 +3,17 @@ import RPi.GPIO as GPIO
 import os
 import time
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(26,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+def setup():
+	GPIO.setwarnings(False)
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(26,GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	GPIO.add_event_detect(26, GPIO.FALLING, callback=send, bouncetime=300)
+
+def send(channel):
+	os.system("echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/bind")
+	#os.system("sudo /opt/vc/bin/tvservice -p")
+
+setup()
 
 while True:
-        input_state= GPIO.input(26)
-	#If button is pressed, turn on usb peripherals and HDMI output
-        if input_state== False:
-                os.system("echo '1-1' |sudo tee /sys/bus/usb/drivers/usb/bind")
-		sudo /opt/vc/bin/tvservice -p
+    time.sleep(6000)
